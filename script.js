cafae29cb7b2ad9d20a66f1ee75cd411473b51c3
Yue JIN <yuejin13@qq.com>
2022-04-08 00:13:57 +0800

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+const start = new Date("2021-04-08 23:55:00").getTime();
+console.log("最喜欢亭宝了！");
+
+setInterval(() => {
+  const dayDom = document.getElementById("day");
+  const hourDom = document.getElementById("hour");
+  const minDom = document.getElementById("min");
+  const secDom = document.getElementById("sec");
+  const now = new Date().getTime();
+  const milisecDiff = now - start;
+  const days = Math.floor(milisecDiff / 1000 / 60 / (60 * 24));
+  const dateDiff = new Date(milisecDiff);
+  dayDom.innerText = days;
+  hourDom.innerText = dateDiff.getHours();
+  minDom.innerText = dateDiff.getMinutes();
+  secDom.innerText = dateDiff.getSeconds();
+}, 1000);
