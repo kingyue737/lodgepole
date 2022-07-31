96846a13c998ad5b0df6e546263ba615322c06f8
Yue JIN <yuejin13@qq.com>
2022-07-31 20:21:40 +0800

    fix: add utc offset
    Co-authored-by: Yuting Zhang <942677256@qq.com>

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const start = new Date("2021-04-08 23:55:00").getTime();
+const utcOffset = new Date().getTimezoneOffset() * 60000;
 console.log("最喜欢亭宝了！");
 
 setInterval(() => {
@@ -7,7 +8,7 @@ setInterval(() => {
   const minDom = document.getElementById("min");
   const secDom = document.getElementById("sec");
   const now = new Date().getTime();
-  const milisecDiff = now - start;
+  const milisecDiff = now - start - utcOffset;
   const days = Math.floor(milisecDiff / 1000 / 60 / (60 * 24));
   const dateDiff = new Date(milisecDiff);
   dayDom.innerText = days;
